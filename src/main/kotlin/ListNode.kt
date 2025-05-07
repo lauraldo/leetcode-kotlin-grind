288449435d9dd158fe4af4b81031694dff42a840
lauraldo <lauraldoiolas@gmail.com>
2025-05-07 14:59:30 +0300

    extract ListNode helper functions

--- src/main/kotlin/ListNode.kt
+++ src/main/kotlin/ListNode.kt
@@ -1,3 +1,32 @@
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
+
+fun IntArray.toLinkedList(cycleIndex: Int = -1): ListNode? {
+    val head = ListNode(this[0])
+    var prev: ListNode = head
+    var cycledNode: ListNode? = null
+    for (i in 1..this.size - 1) {
+        val node = ListNode(this[i])
+        if (i == cycleIndex) {
+            cycledNode = node
+        }
+        prev.next = node
+        prev = node
+    }
+    cycledNode?.let {
+        prev.next = it
+    }
+    return head
+}
+
+fun ListNode.stringify(): String {
+    var current: ListNode? = this
+    val builder = StringBuilder()
+    do {
+        builder.append(current?.`val`)
+        builder.append(", ")
+        current = current?.next
+    } while (current != null)
+    return builder.toString()
+}
--- src/main/kotlin/MergeTwoSortedLists.kt
+++ src/main/kotlin/MergeTwoSortedLists.kt
@@ -53,35 +53,6 @@ fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
     }
 }
 
-fun IntArray.toLinkedList(cycleIndex: Int = -1): ListNode? {
-    val head = ListNode(this[0])
-    var prev: ListNode = head
-    var cycledNode: ListNode? = null
-    for (i in 1..this.size - 1) {
-        val node = ListNode(this[i])
-        if (i == cycleIndex) {
-            cycledNode = node
-        }
-        prev.next = node
-        prev = node
-    }
-    cycledNode?.let {
-        prev.next = it
-    }
-    return head
-}
-
-fun ListNode.stringify(): String {
-    var current: ListNode? = this
-    val builder = StringBuilder()
-    do {
-        builder.append(current?.`val`)
-        builder.append(", ")
-        current = current?.next
-    } while (current != null)
-    return builder.toString()
-}
-
 fun main() {
     val list1 = intArrayOf(1, 2, 4).toLinkedList()
     println("list1 = ${list1?.stringify()}")
